27319551ec1e777fe1f417ca32419b5a1ce63064
Lorenz De Robles <pvd8@txstate.edu>
2024-01-30 15:28:36 -0600

    deleted comment

--- meeting_1/level_1/ascii_art/main.cpp
+++ meeting_1/level_1/ascii_art/main.cpp
@@ -1,10 +1,4 @@
-/* The objective of this mini project is to collaborate and learn. 
-The provided code is a starting point and its purpose is to help start the process. 
-Your task is to fill in the missing code and implement the missing pieces
-Feel free to modify the code extensively.
-Don't be afraid to use the internet or ask for help.
-Most importantly have fun!
-*/
+// "Solution" for ascii_art
 #include<iostream>
 using namespace std;
 
